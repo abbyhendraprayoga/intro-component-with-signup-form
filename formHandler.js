602fb1c9216bf2649f03bb7a678c170554f1e7f0
abbyhendraprayoga <voxartzyt@gmail.com>
2024-08-12 18:04:11 +0800

    adding validation success icon and fix bug

--- formHandler.js
+++ formHandler.js
@@ -11,8 +11,10 @@ const errorFn = document.querySelector("#error-fn");
 const errorLn = document.querySelector("#error-ln");
 const errorEm = document.querySelector("#error-em");
 const errorPw = document.querySelector("#error-pw");
+const eyeIcon = document.querySelector("#eye-icon");
 const allInput = document.querySelector(".input");
-const symbols = /[!@#$%^&*()_+{}\[\]:;"'<>,.?/~`1234567890]/;
+const symbolsName = /[!@#$%^&*()_+{}\[\]:;"'<>,.?/~`1234567890]/;
+const symbolsEmail = /[!#$%^&*()_+{}\[\]:;"'<>,?/~`]/;
 
 function validateForm() {
   if (firstNameInput.value.trim() === "") {
@@ -21,6 +23,11 @@ function validateForm() {
     errorIconFn.classList.add("error-icon");
     errorFn.classList.remove("d-none");
     firstNameInput.classList.remove("mb-3");
+    firstNameInput.classList.remove("success-input");
+    firstNameInput.classList.add("danger-input");
+    errorFn.classList.add("text-danger");
+    errorFn.classList.remove("text-success");
+    errorIconFn.src = "./images/icon-error.svg";
     errorFn.innerHTML = "First name cannot be empty";
   } else if (firstNameInput.value.length < 3) {
     errorIconFn.classList.remove("d-none");
@@ -28,18 +35,33 @@ function validateForm() {
     errorIconFn.classList.add("error-icon-2");
     errorFn.classList.remove("d-none");
     firstNameInput.classList.remove("mb-3");
+    firstNameInput.classList.remove("success-input");
+    firstNameInput.classList.add("danger-input");
+    errorFn.classList.remove("text-success");
+    errorFn.classList.add("text-danger");
+    errorIconFn.src = "./images/icon-error.svg";
     errorFn.innerHTML = "First name must be at least 3 characters";
-  } else if (symbols.test(firstNameInput.value)) {
+  } else if (symbolsName.test(firstNameInput.value)) {
     errorIconFn.classList.remove("d-none");
     errorIconFn.classList.remove("error-icon");
     errorIconFn.classList.add("error-icon-2");
     errorFn.classList.remove("d-none");
+    errorFn.classList.remove("text-success");
+    errorFn.classList.add("text-danger");
     firstNameInput.classList.remove("mb-3");
+    firstNameInput.classList.remove("success-input");
+    firstNameInput.classList.add("danger-input");
+    errorIconFn.src = "./images/icon-error.svg";
     errorFn.innerHTML = "First name cannot contain symbols or numbers!";
   } else {
-    firstNameInput.classList.add("mb-3");
-    errorIconFn.classList.add("d-none");
-    errorFn.classList.add("d-none");
+    firstNameInput.classList.remove("mb-3");
+    firstNameInput.classList.remove("danger-input");
+    firstNameInput.classList.add("success-input");
+    errorIconFn.classList.remove("d-none");
+    errorFn.classList.add("text-success");
+    errorFn.classList.remove("text-danger");
+    errorFn.classList.remove("d-none");
+    errorIconFn.src = "./images/icon-success.svg";
     errorFn.textContent = "Success!";
   }
 
@@ -48,26 +70,46 @@ function validateForm() {
     errorIconLn.classList.remove("error-icon-2");
     errorIconLn.classList.add("error-icon");
     errorLn.classList.remove("d-none");
+    lastNameInput.classList.add("danger-input");
+    lastNameInput.classList.remove("success-input");
     lastNameInput.classList.remove("mb-3");
+    errorLn.classList.add("text-danger");
+    errorLn.classList.remove("text-success");
+    errorIconLn.src = "./images/icon-error.svg";
     errorLn.textContent = "Last name cannot be empty";
   } else if (lastNameInput.value.length < 3) {
     errorIconLn.classList.remove("d-none");
     errorIconLn.classList.remove("error-icon");
     errorIconLn.classList.add("error-icon-2");
-    errorLn.classList.remove("d-none");
+    lastNameInput.classList.add("danger-input");
+    lastNameInput.classList.remove("success-input");
     lastNameInput.classList.remove("mb-3");
+    errorLn.classList.remove("d-none");
+    errorLn.classList.remove("text-success");
+    errorLn.classList.add("text-danger");
     errorLn.innerHTML = "Last name must be at least 3 characters";
-  } else if (symbols.test(lastNameInput.value)) {
+    errorIconLn.src = "./images/icon-error.svg";
+  } else if (symbolsName.test(lastNameInput.value)) {
     errorIconLn.classList.remove("d-none");
     errorIconLn.classList.remove("error-icon");
     errorIconLn.classList.add("error-icon-2");
     errorLn.classList.remove("d-none");
+    errorLn.classList.remove("text-success");
+    errorLn.classList.add("text-danger");
+    lastNameInput.classList.add("danger-input");
+    lastNameInput.classList.remove("success-input");
     lastNameInput.classList.remove("mb-3");
+    errorIconLn.src = "./images/icon-error.svg";
     errorLn.innerHTML = "Last name cannot contain symbols or numbers!";
   } else {
-    lastNameInput.classList.add("mb-3");
-    errorIconLn.classList.add("d-none");
-    errorLn.classList.add("d-none");
+    lastNameInput.classList.remove("mb-3");
+    lastNameInput.classList.remove("danger-input");
+    lastNameInput.classList.add("success-input");
+    errorIconLn.classList.remove("d-none");
+    errorLn.classList.remove("text-danger");
+    errorLn.classList.add("text-success");
+    errorLn.classList.remove("d-none");
+    errorIconLn.src = "./images/icon-success.svg";
     errorLn.textContent = "Success!";
   }
 
@@ -77,21 +119,39 @@ function validateForm() {
     errorIconEm.classList.add("error-icon");
     errorEm.classList.remove("d-none");
     emailInput.classList.remove("mb-3");
+    emailInput.classList.add("danger-input");
+    emailInput.classList.remove("success-input");
+    errorEm.classList.remove("text-success");
+    errorEm.classList.add("text-danger");
+    errorEm.classList.remove("text-success");
+    errorIconEm.src = "./images/icon-error.svg";
     errorEm.textContent = "Email cannot be empty";
   } else if (
     !emailInput.value.includes("@") ||
-    !emailInput.value.includes(".com")
+    !emailInput.value.includes(".com") ||
+    symbolsEmail.test(emailInput.value)
   ) {
     errorIconEm.classList.remove("d-none");
     errorIconEm.classList.remove("error-icon");
     errorIconEm.classList.add("error-icon-2");
     errorEm.classList.remove("d-none");
     emailInput.classList.remove("mb-3");
+    emailInput.classList.remove("success-input");
+    emailInput.classList.add("danger-input");
+    errorEm.classList.remove("text-success");
+    errorEm.classList.add("text-danger");
+    errorIconEm.src = "./images/icon-error.svg";
     errorEm.innerHTML = "Looks like this is not an email";
   } else {
-    emailInput.classList.add("mb-3");
-    errorIconEm.classList.add("d-none");
-    errorEm.classList.add("d-none");
+    emailInput.classList.remove("mb-3");
+    emailInput.classList.remove("danger-input");
+    emailInput.classList.add("success-input");
+    // errorIconEm.classList.remove("d-none");
+    errorEm.classList.remove("text-danger");
+    errorEm.classList.add("text-success");
+    errorIconEm.classList.remove("d-none");
+    errorEm.classList.remove("d-none");
+    errorIconEm.src = "./images/icon-success.svg";
     errorEm.textContent = "Success!";
   }
 
@@ -101,25 +161,53 @@ function validateForm() {
     errorIconPw.classList.add("error-icon");
     errorPw.classList.remove("d-none");
     passwordInput.classList.remove("mb-3");
+    passwordInput.classList.add("danger-input");
+    passwordInput.classList.remove("success-input");
+    errorPw.classList.remove("text-success");
+    errorPw.classList.add("text-danger");
+    eyeIcon.classList.add("eye-icon2");
+    eyeIcon.classList.remove("eye-icon");
+    errorIconPw.src = "./images/icon-error.svg";
     errorPw.textContent = "Password cannot be empty";
   } else if (passwordInput.value.length < 6) {
     errorIconPw.classList.remove("d-none");
     errorIconPw.classList.remove("error-icon");
     errorIconPw.classList.add("error-icon-2");
     errorPw.classList.remove("d-none");
+    eyeIcon.classList.add("eye-icon2");
+    eyeIcon.classList.remove("eye-icon");
     passwordInput.classList.remove("mb-3");
+    passwordInput.classList.remove("success-input");
+    passwordInput.classList.add("danger-input");
+    errorPw.classList.remove("text-success");
+    errorPw.classList.add("text-danger");
+    errorIconPw.src = "./images/icon-error.svg";
     errorPw.innerHTML = "Password must be at least 6 character";
   } else if (!/[A-Z]/.test(passwordInput.value)) {
     errorIconPw.classList.remove("d-none");
     errorIconPw.classList.remove("error-icon");
     errorIconPw.classList.add("error-icon-2");
     errorPw.classList.remove("d-none");
+    eyeIcon.classList.add("eye-icon2");
+    eyeIcon.classList.remove("eye-icon");
     passwordInput.classList.remove("mb-3");
+    passwordInput.classList.remove("success-input");
+    passwordInput.classList.add("danger-input");
+    errorPw.classList.remove("text-success");
+    errorPw.classList.add("text-danger");
+    errorIconPw.src = "./images/icon-error.svg";
     errorPw.innerHTML = "Password must contain a uppercase letter";
   } else {
-    passwordInput.classList.add("mb-3");
-    errorIconPw.classList.add("d-none");
-    errorPw.classList.add("d-none");
+    passwordInput.classList.remove("mb-3");
+    errorIconPw.classList.remove("d-none");
+    errorPw.classList.remove("d-none");
+    eyeIcon.classList.add("eye-icon2");
+    eyeIcon.classList.remove("eye-icon");
+    passwordInput.classList.remove("danger-input");
+    passwordInput.classList.add("success-input");
+    errorPw.classList.remove("text-danger");
+    errorPw.classList.add("text-success");
+    errorIconPw.src = "./images/icon-success.svg";
     errorPw.textContent = "Success!";
   }
 }
@@ -135,6 +223,17 @@ function resetForm() {
     location.reload();
   }
 }
+
+eyeIcon.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.src = "./images/eye-hide.svg";
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.src = "./images/eye.svg";
+  }
+});
 
 signUpForm.addEventListener("submit", (event) => {
   event.preventDefault();
